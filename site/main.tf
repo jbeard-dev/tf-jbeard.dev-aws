23bc1f4f6c9070d4e2513d2e22453bcdc026cd7c
Josh Beard <josh@joshbeard.me>
2022-03-15 17:14:55 -0600

    Linting

--- site/main.tf
+++ site/main.tf
@@ -1,16 +1,16 @@
 # Deployment of AWS resources for jbeard.dev
 module "jbeard_dev_aws" {
-    source = "github.com/joshbeard/tf-aws-site.git"
+  source = "github.com/joshbeard/tf-aws-site.git"
 
-    domain      = "jbeard.dev"
-    bucket_name = "s3-website-jbeard-dev"
-    iam_name    = "s3-deployer-jbeard-dev"
+  domain      = "jbeard.dev"
+  bucket_name = "s3-website-jbeard-dev"
+  iam_name    = "s3-deployer-jbeard-dev"
 }
 
 module "jbeard_dev_migadu" {
-    source = "github.com/joshbeard/tf-migadu-route53.git"
+  source = "github.com/joshbeard/tf-migadu-route53.git"
 
-    domain        = "jbeard.dev"
-    zone_id       = module.jbeard_dev_aws.zone_id
-    migadu_verify = "3zbtwhss"
+  domain        = "jbeard.dev"
+  zone_id       = module.jbeard_dev_aws.route53_zone_id
+  migadu_verify = "3zbtwhss"
 }
